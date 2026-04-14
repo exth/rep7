ecba66c7383da6b06d00a232eb90d8fd1064f687
clxwly <lxwlyyy@gmail.com>
2026-04-14 03:31:15 +0300

    Initial XCode project - CREATE

---
+++ gitTestRep7/gitTestRep7/gitTestRep7App.swift
@@ -0,0 +1,17 @@
+//
+//  gitTestRep7App.swift
+//  gitTestRep7
+//
+//  Created by Вадим on 14.04.26.
+//
+
+import SwiftUI
+
+@main
+struct gitTestRep7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
